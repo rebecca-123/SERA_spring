081442bda0292e0ea4328e7d34affd7d3071c740
Rebecca-123 <rebecca.notw@gmail.com>
2023-02-02 18:11:55 -0800

    minor update

--- src/main/java/com/nighthawk/team_backend/mvc/database/club/ClubJpaRepository.java
+++ src/main/java/com/nighthawk/team_backend/mvc/database/club/ClubJpaRepository.java
@@ -10,7 +10,7 @@ Extends the JpaRepository interface from Spring Data JPA.
 -- Java Persistent API (JPA) - Hibernate: map, store, update and retrieve database
 -- JpaRepository defines standard CRUD methods
 -- Via JPA the developer can retrieve database from relational databases to Java objects and vice versa.
-*/
+ */
 public interface ClubJpaRepository extends JpaRepository<Club, Long> {
   Club findByEmail(String email);
 
@@ -19,12 +19,14 @@ public interface ClubJpaRepository extends JpaRepository<Club, Long> {
   // JPA query, findBy does JPA magic with "Name", "Containing", "Or", "Email",
   // "IgnoreCase"
   List<Club> findByNameContainingIgnoreCaseOrEmailContainingIgnoreCase(String name, String email);
+
   /*
    * Custom JPA query articles, there are articles that show custom SQL as well
    * https://springframework.guru/spring-data-jpa-query/
    * https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#jpa.query
    * -methods
    */
+  Club findByEmailAndPassword(String email, String password);
 
   // Custom JPA query
   @Query(value = "SELECT * FROM Club p WHERE p.name LIKE ?1 or p.email LIKE ?1", nativeQuery = true)
